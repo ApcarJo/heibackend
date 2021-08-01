a68707ee392ef11f8f1d8f47c993ca1864b378af
ApcarJo <hansokaz@gmail.com>
2021-08-01 14:22:55 +0200

    created models, migrations and controllers

---
+++ app/Http/Controllers/PassportAuthController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\technicalguarantee;
+
+class PassportAuthController extends Controller
+{
+    //
+
+    public function register(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required|min:8'
+        ]);
+    
+
+        $tg = technicalguarantee::create([
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        $token = $tg->createToken('LaravelAuthApp')->accessToken;
+
+        return response()->json(['token' => $token], 200);
+    }
+
+    public function login (Request $request)
+    {
+        $data = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+
+        if (auth()->attempt($data)) {
+            $token = auth()->user()->createToken('LaravelAuthApp')->accessToken;
+            return response()->json(['token' => $token], 200);
+        } else {
+            return response()->json(['error' => 'Unauthorised'], 401);
+        }
+    }
+}
