3ba07c6024b699b63947cab5dcfce029288b3381
José Luis <hansokaz@gmail.com>
2021-08-24 23:44:35 +0200

    adding cors syntax

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,7 +16,9 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        return $next($request);
-        header('Access-Control-Allow-Origin', '*');
+        return $next($request)
+        ->header('Access-Control-Allow-Origin', '*')
+        ->header('Access-Control-Allow-Methods', 'PUT, POST, DELETE, GET')
+        ->header('Access-Control-Allow-Header', 'Accept,Authorization,Content-Type');
     }
 }
