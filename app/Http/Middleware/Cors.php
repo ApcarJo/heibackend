87e83141f13fe73f3f8d924f3cdba3cd270ceeda
José Luis <hansokaz@gmail.com>
2021-08-25 19:04:08 +0200

    added one header to handle cors.php

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -20,10 +20,9 @@ class Cors
         // ->header('Access-Control-Allow-Origin', '*')
         // ->header('Access-Control-Allow-Methods', 'PUT, POST, DELETE, GET')
         // ->header('Access-Control-Allow-Header', 'Accept,Authorization,Content-Type');
-        ->header('Access-Control-Allow-Origin', '*')
-      ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-      ->header('Access-Control-Allow-Headers',' Origin, Content-Type, Accept, Authorization, X-Request-With')
-      ->header('Access-Control-Allow-Credentials',' true');
-}
+        header('Access-Control-Allow-Origin', '*')
+        header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+        header('Access-Control-Allow-Headers',' Origin, Content-Type, Accept, Authorization, X-Request-With')
+        header('Access-Control-Allow-Credentials',' true');
     }
 }
