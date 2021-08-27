bc8ebce582f52c526d71bfb43610f6104a1b8d5f
José Luis <hansokaz@gmail.com>
2021-08-27 13:38:32 +0200

    changed cors.php handle headers

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,13 +16,13 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        return $next($request);
+        return $next($request)
         // ->header('Access-Control-Allow-Origin', '*')
         // ->header('Access-Control-Allow-Methods', 'PUT, POST, DELETE, GET')
         // ->header('Access-Control-Allow-Header', 'Accept,Authorization,Content-Type');
-        header('Access-Control-Allow-Origin', 'http://heifrontend.herokuapp.com/');
-        header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        header('Access-Control-Allow-Headers',' Origin, Content-Type, Accept, Authorization, X-Request-With');
-        header('Access-Control-Allow-Credentials',' true');
+        ->header('Access-Control-Allow-Origin', 'http://heifrontend.herokuapp.com/')
+        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+        ->header('Access-Control-Allow-Headers',' Origin, Content-Type, Accept, Authorization, X-Request-With')
+        ->header('Access-Control-Allow-Credentials',' true');
     }
 }
