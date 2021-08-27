025185d341fb78b3f9b242635c519ef47b1db738
José Luis <hansokaz@gmail.com>
2021-08-27 19:02:22 +0200

    added line 31 to passposrt controller

--- app/Http/Controllers/PassportAuthController.php
+++ app/Http/Controllers/PassportAuthController.php
@@ -28,7 +28,8 @@ class PassportAuthController extends Controller
 
         $token = $user->createToken('LaravelAuthApp')->accessToken;
 
-        return response()->json(['token' => $token], 200);
+        return Response::make('OK', 200, $headers)
+        response()->json(['token' => $token], 200);
 
     }
     
