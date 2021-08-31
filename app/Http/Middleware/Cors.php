2bf0e3841bc6460fa3ada30e7787c1d74e3f609e
José Luis <hansokaz@gmail.com>
2021-08-31 13:46:24 +0200

    cors from David

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,14 +17,20 @@ class Cors
     public function handle($request, Closure $next)
     {
         return $next($request)
+        // // ->header('Access-Control-Allow-Origin', '*')
+        // // ->header('Access-Control-Allow-Methods', 'PUT, POST, DELETE, GET')
+        // // ->header('Access-Control-Allow-Header', 'Accept,Authorization,Content-Type');
         // ->header('Access-Control-Allow-Origin', '*')
-        // ->header('Access-Control-Allow-Methods', 'PUT, POST, DELETE, GET')
-        // ->header('Access-Control-Allow-Header', 'Accept,Authorization,Content-Type');
-        ->header('Access-Control-Allow-Origin', '*')
-        // ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-        ->header('Access-Control-Allow-Methods', '*')
-        ->header('Access-Control-Allow-Headers', '*')
-        // ->header('Access-Control-Allow-Headers',' Origin, Content-Type, Accept, Authorization, X-Request-With')
-        ->header('Access-Control-Allow-Credentials','true');
+        // // ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+        // ->header('Access-Control-Allow-Methods', '*')
+        // ->header('Access-Control-Allow-Headers', '*')
+        // // ->header('Access-Control-Allow-Headers',' Origin, Content-Type, Accept, Authorization, X-Request-With')
+        // ->header('Access-Control-Allow-Credentials','true');
+        //Url a la que se le dará acceso en las peticiones
+      ->header("Access-Control-Allow-Origin", "*")
+      //Métodos que a los que se da acceso
+      ->header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+      //Headers de la petición
+      ->header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, X-Token-Auth, Authorization"); 
     }
 }
